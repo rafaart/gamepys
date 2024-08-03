88f6298d9659a4307cbf897ee7c043df5f467ea7
rafaart <rafaartsantos@gmail.com>
2024-08-03 20:12:47 -0300

    <feature>[player]: adicionado movimento de sprite e teste de colisão

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 # pega uma lista de todas as resoluções suportadas
 resolutions = pygame.display.list_modes()
 
@@ -10,14 +13,14 @@ first_resolution = resolutions[0]
 width, height = first_resolution
 
 # modifica a resolução da screen para ser quadrada
-width = width*0.4
-height = width
+width = height*0.5
+height = height*0.5
 
 # define o tamanho dos blocos do cenário
-
 brick_size = width * 0.05
 
 print("Resolução da tela atual:", width, "x", height)
+print("tamanho do brick: ", brick_size)
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Parkour')
@@ -27,23 +30,104 @@ sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 
 
-def draw_grid():
-    for line in range(0, 20):
-        pygame.draw.line(screen, (255, 255, 255), (0, line *
-                         brick_size), (width, line * brick_size))
-        pygame.draw.line(screen, (255, 255, 255), (line *
-                         brick_size, 0), (line * brick_size, height))
-
-
 class Player():
     def __init__(self, x, y):
-        img = pygame.image.load('img/player.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 12):
+            img_right = pygame.image.load(f'img/p1_walk{num}.png')
+            img_right = pygame.transform.scale(
+                img_right, (brick_size*0.9, brick_size * 2))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.vel_y = 0
+        self.jumped = False
+        self.direction = 0
+
+        print("a largura e a altura da imagem: ",
+              self.width, " x ", self.height)
 
     def update(self):
+
+        dx = 0
+        dy = 0
+        walk_delay = 2
+
+        # pegando teclado
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -5
+            self.jumped = True
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+            self.counter += 1
+            self.direction = -1
+        if key[pygame.K_RIGHT]:
+            dx += 5
+            self.counter += 1
+            self.direction = 1
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # animação do sprite
+        if self.counter > walk_delay:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
+
+        # gravidade
+        self.vel_y += 0.1
+        if self.vel_y > 5:
+            self.vel_y = 5
+        dy += self.vel_y
+
+        # checar colisão
+        for tile in world.tile_list:
+
+            # chequar colisão na direção x
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                dx = 0
+
+            # chequar por colisão na direção y
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # chequar se abaixo do solo (pulando)
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top
+                    self.vel_y = 0
+                # chequar se acima do solo (caindo)
+                elif self.vel_y > 0:
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
+        # atualizar posição do jogador
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+            dy = 0
+
+        # desenha o jogador na tela
         screen.blit(self.image, self.rect)
 
 
@@ -106,20 +190,19 @@ world_data = [
     [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-player = Player(10, height - 130)
+player = Player(100, height - (brick_size + brick_size*2))
 world = World(world_data)
 
 run = True
 while run:
 
+    clock.tick(fps)
     screen.blit(bg_img, (0, 0))
     screen.blit(sun_img, (100, 100))
 
     world.draw()
 
     player.update()
-
-    draw_grid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

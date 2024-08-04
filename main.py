887ee5906bea52accf39ca13dc64ec06cca8faf9
rafaart <rafaartsantos@gmail.com>
2024-08-04 00:27:04 -0300

    <fix>[player]: concerto do pulo infinito<feature>[restart]: adicionado butão de reinicio

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ height = height*0.5
 
 # define o tamanho dos blocos do cenário
 brick_size = width * 0.05
+game_over = 0
 
 print("Resolução da tela atual:", width, "x", height)
 print("tamanho do brick: ", brick_size)
@@ -28,10 +29,131 @@ pygame.display.set_caption('Parkour')
 # carregar imagens
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
+restart_img = pygame.image.load('img/restart.png')
+
+
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.clicked = False
+
+    def draw(self):
+
+        action = False
+
+        # pega a posição do cursor
+        pos = pygame.mouse.get_pos()
+
+        # checka colisão do cursor
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                action = True
+                self.clicked = True
+
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+
+        # desenha o butão
+        screen.blit(self.image, self.rect)
+
+        return action
 
 
 class Player():
     def __init__(self, x, y):
+        self.reset(x, y)
+
+        print("a largura e a altura da imagem: ",
+              self.width, " x ", self.height)
+
+    def update(self, game_over):
+
+        dx = 0
+        dy = 0
+        walk_delay = 2
+
+        if game_over == 0:
+            # pegando teclado
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+                self.vel_y = -5
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+                self.counter += 1
+                self.direction = -1
+            if key[pygame.K_RIGHT]:
+                dx += 5
+                self.counter += 1
+                self.direction = 1
+            if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+                self.counter = 0
+                self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+            # animação do sprite
+            if self.counter > walk_delay:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+
+            # gravidade
+            self.vel_y += 0.1
+            if self.vel_y > 5:
+                self.vel_y = 5
+            dy += self.vel_y
+
+            # checar colisão
+            self.in_air = True
+            for tile in world.tile_list:
+
+                # chequar colisão na direção x
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+
+                # chequar por colisão na direção y
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    # chequar se abaixo do solo (pulando)
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    # chequar se acima do solo (caindo)
+                    elif self.vel_y > 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+                        self.in_air = False
+
+            # checkar por colisões com o inimigo
+            if pygame.sprite.spritecollide(self, snail_group, False):
+                game_over = -1
+
+            # atualizar posição do jogador
+            self.rect.x += dx
+            self.rect.y += dy
+
+        elif game_over == -1:
+            self.image = self.hurt_image
+            if self.rect.y > 0:
+                self.rect.y += 5
+
+        # desenha o jogador na tela
+        screen.blit(self.image, self.rect)
+        return game_over
+
+    def reset(self, x, y):
         self.images_right = []
         self.images_left = []
         self.index = 0
@@ -43,6 +165,7 @@ class Player():
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
+        self.hurt_image = pygame.image.load('img/p1_hurt.png')
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -52,83 +175,7 @@ class Player():
         self.vel_y = 0
         self.jumped = False
         self.direction = 0
-
-        print("a largura e a altura da imagem: ",
-              self.width, " x ", self.height)
-
-    def update(self):
-
-        dx = 0
-        dy = 0
-        walk_delay = 2
-
-        # pegando teclado
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and self.jumped == False:
-            self.vel_y = -5
-            self.jumped = True
-        if key[pygame.K_SPACE] == False:
-            self.jumped = False
-        if key[pygame.K_LEFT]:
-            dx -= 5
-            self.counter += 1
-            self.direction = -1
-        if key[pygame.K_RIGHT]:
-            dx += 5
-            self.counter += 1
-            self.direction = 1
-        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
-            self.counter = 0
-            self.index = 0
-            if self.direction == 1:
-                self.image = self.images_right[self.index]
-            if self.direction == -1:
-                self.image = self.images_left[self.index]
-
-        # animação do sprite
-        if self.counter > walk_delay:
-            self.counter = 0
-            self.index += 1
-            if self.index >= len(self.images_right):
-                self.index = 0
-            if self.direction == 1:
-                self.image = self.images_right[self.index]
-            if self.direction == -1:
-                self.image = self.images_left[self.index]
-
-        # gravidade
-        self.vel_y += 0.1
-        if self.vel_y > 5:
-            self.vel_y = 5
-        dy += self.vel_y
-
-        # checar colisão
-        for tile in world.tile_list:
-
-            # chequar colisão na direção x
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-
-            # chequar por colisão na direção y
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                # chequar se abaixo do solo (pulando)
-                if self.vel_y < 0:
-                    dy = tile[1].bottom - self.rect.top
-                    self.vel_y = 0
-                # chequar se acima do solo (caindo)
-                elif self.vel_y > 0:
-                    dy = tile[1].top - self.rect.bottom
-                    self.vel_y = 0
-        # atualizar posição do jogador
-        self.rect.x += dx
-        self.rect.y += dy
-
-        if self.rect.bottom > height:
-            self.rect.bottom = height
-            dy = 0
-
-        # desenha o jogador na tela
-        screen.blit(self.image, self.rect)
+        self.in_air = True
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -220,6 +267,9 @@ snail_group = pygame.sprite.Group()
 
 world = World(world_data)
 
+# cria os butões
+restart_button = Button(width // 2, height // 2, restart_img)
+
 run = True
 while run:
 
@@ -229,10 +279,17 @@ while run:
 
     world.draw()
 
-    snail_group.update()
+    if game_over == 0:
+        snail_group.update()
+
     snail_group.draw(screen)
 
-    player.update()
+    game_over = player.update(game_over)
+
+    if game_over == -1:
+        if restart_button.draw():
+            player.reset(100, height - (brick_size + brick_size*2))
+            game_over = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
